b636be01dd26cd70bbea26b7563107f0f455c627
LoneExile <Apinant-Moo@tutanota.com>
2023-03-26 08:00:27 +0700

    feat: update encoder keys depend on layer

--- keyboards/bunlaikun/keymaps/default/features/encoder.c
+++ keyboards/bunlaikun/keymaps/default/features/encoder.c
@@ -1,25 +1,20 @@
-
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    // 0 and 1 are left-half encoders
-    // 2 and 3 are right-half encoders
-    // if (index == 0) {
-    //     tap_code(KC_0);
-    // } else if (index == 1) {
-    //     tap_code(KC_1);
-    // } else if (index == 2) {
-    //     tap_code(KC_2);
-    // } else if (index == 3) {
-    //     tap_code(KC_3);
-    // }
+    // index 0 and 1 are left-half encoders
+    // index 2 and 3 are right-half encoders
 
-    if (clockwise) {
-        // tap_code16(KC_PLUS);
-        tap_code(KC_VOLU);
+    uint16_t key;
+    if (layer_state_is(_BASE)) {
+        key = clockwise ? KC_VOLU : KC_VOLD;
+    } else if (layer_state_is(_NAV)) {
+        key = clockwise ? KC_WH_U : KC_WH_D;
     } else {
-        // tap_code(KC_MINUS);
-        tap_code(KC_VOLD);
+        key = clockwise ? KC_VOLU : KC_VOLD;
+        // return false; // Do nothing if no match
     }
+
+    tap_code(key);
+
 
     return false;
 }
